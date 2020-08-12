5adb19c3cb69dde17f76dae416fe91b87874849e
Brallam <Braianifernandez87@hotmail.com>
2020-08-11 22:54:30 -0300

    carrito alpha

--- site/app.js
+++ site/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var newProduct = require('./routes/newProduct')
 var productRouter=require("./routes/productDetails")
+var carritoRouter=require("./routes/carrito")
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/newProduct", newProduct)
 app.use("/products", productRouter)
+app.use("/carrito",carritoRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
