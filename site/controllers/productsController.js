cd5d62c46703aec0bdffee20e06cb6e1a93e08fa
Brallam <Braianifernandez87@hotmail.com>
2020-08-23 15:07:31 -0300

    editado titulos ejs

--- site/controllers/productsController.js
+++ site/controllers/productsController.js
@@ -1,21 +1,21 @@
 let dbProduct = require('../data/dataBase') 
 
 module.exports={
-
     pruebaVista:function(req,res,next){
         res.render('productDetails',{
-            
+            title:"Detalle",
             productos: dbProduct
         })
     },
     detalle:function(req,res,next){
+        
         let id= req.params.id;
         let producto = dbProduct.filter(producto=>{
             return id==producto.id
         })
         console.log(producto)
         res.render('productDetails',{
-            
+            title:"Detalle",
             producto:producto[0]
         })
     }
