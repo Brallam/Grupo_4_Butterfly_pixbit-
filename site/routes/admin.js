e2134d51ca19eac8e8a0fc936b05ccc08218ceb6
Angel Gimenez <angelgimenez992@gmail.com>
2020-08-27 00:33:30 -0300

    multer y propiedad agragada

--- site/routes/admin.js
+++ site/routes/admin.js
@@ -3,9 +3,23 @@ var router = express.Router();
 
 var controller = require("../controllers/adminController")
 
+const multer = require('multer');
+const path = require('path')
+
+let storage = multer.diskStorage({
+    destination:(req,file,callback)=>{
+        callback(null,'public/images/products')
+    },
+    filename:(req,file,callback)=>{
+        callback(null,file.fieldname + Date.now() + path.extname(file.originalname))
+    }
+})
+
+let upload = multer({storage:storage})
+
 router.get('/', controller.lista);
 router.get('/newProduct', controller.mostrarForm);
-router.post('/newProduct', controller.publicar)
+router.post('/newProduct',upload.any(), controller.publicar)
 router.get('/editproduct/:id', controller.edit);
 router.post('/editproduct/:id', controller.editp)
 
