47fe70c388b2f15fc4c87473fc78c6d6681b396a
santiago-88 <peppers.santi@gmail.com>
2020-09-14 23:48:06 -0300

    prueba

--- site/validators/loginValidator.js
+++ site/validators/loginValidator.js
@@ -5,23 +5,6 @@ const bcrypt = require ('bcrypt')
 let dbUsers = require('../data/databaseUsers');
 
 module.exports = [
-    check('email').trim().isEmail().withMessage('El email ingresado es invalido'),
+    check('email').trim().isEmail().withMessage('El email ingresado es invalido')
 
-    body('pass')
-    .custom(function(value,{req}){
-        let result = true;
-        dbUsers.forEach(user => {
-            if(user.email == req.body.email){
-                if(!bcrypt.compareSync(value,user.password)){
-                    result = false
-                }
-            }
-        });
-        if (result == false){
-            return false
-        }else{
-            return true
-        }
-    })
-    .withMessage("Contraseña incorrecta")
-]
+     ]
