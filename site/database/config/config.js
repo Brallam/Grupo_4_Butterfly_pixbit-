afe96e494ac8c490acbf8bd3a87db869c2c2aa25
santiago-88 <peppers.santi@gmail.com>
2020-10-29 15:20:13 -0300

    validaciones registe en front

--- site/database/config/config.js
+++ site/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "Butterfly_pixbit_v2",
+    "database": "butterfly-pix-bit",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "port":3306
