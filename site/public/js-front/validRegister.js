f4b2526ea4183e5e664a53ad8ea18fc939527324
Angel Gimenez <angelgimenez992@gmail.com>
2020-11-03 10:49:54 -0300

    corrije email validator

--- site/public/js-front/validRegister.js
+++ site/public/js-front/validRegister.js
@@ -1,69 +1,59 @@
-window.addEventListener('load', function() {
-let divErrores = document.getElementById('cajaErrores');
-let form = document.getElementById('formulario');
-let name = document.getElementById('name')
-let nameU = document.getElementById('nameU') 
-let email = document.getElementById('email')
-let pass = document.getElementById('pwd')
-let pass2 = document.getElementById('pwd2')
-
-
-let emailregex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-let passregex = /^(?=.*\d)(?=.*[a-záéíóúüñ]).*[A-ZÁÉÍÓÚÜÑ]/;
-
-let validateImputs = ()=>{
-let errors = []
-
-   
-if(name.value.length<2){
-    errors.push(' El nombre debe tener al menos 2 caracteres')
-}
-if(nameU.value.length<2){
-    errors.push(' El nombre de usuario debe tener al menos 2 caracteres')
-}
-if(!emailregex.test(email.value)){
-    errors.push(' El formato de el email es inválido')
-}
-if(!passregex.test(pass.value)){
-    errors.push('La contraseña debe contener mayúscula y un número y una minuscula')
-}
-if(pass.value!=pass2.value){
-    errors.push('Las contraseñas deben ser iguales')
-}
-
-return errors
-}
-
-form.onsubmit=function(e){
+window.addEventListener('load', function () {
     fetch('http://localhost:3030/api/users')
-.then(function(respuesta){
-    return respuesta.json()
-})
-    .then(function(info){
-        
-        info.forEach(function(data){
-            if(data==email.value){
-                    e.preventDefault()
-                    divErrores.innerHTML +=
-                '<div class="alert alert-danger" role="alert">El email está registrado</div>'
-            console.log('email ya registrado ')     
-        }
+        .then(function (respuesta) {
+            return respuesta.json()
         })
-})
-    console.log(email.value)
-    e.preventDefault()
-     divErrores.innerHTML = "";
-    let errores = validateImputs()
-    console.log(errores);
-     if(errores.length>0){  
-                e.preventDefault()
-                    errores.forEach(error => {
-                     divErrores.innerHTML +=
-                    '<div class="alert alert-danger" role="alert">'+error+'</div>' 
-        })
-    }else{
-    form.submit()
-    }
-}
-    });
+        .then(function (info) {
+            let divErrores = document.getElementById('cajaErrores');
+            let form = document.getElementById('formulario');
+            let name = document.getElementById('name')
+            let nameU = document.getElementById('nameU')
+            let email = document.getElementById('email')
+            let pass = document.getElementById('pwd')
+            let pass2 = document.getElementById('pwd2')
 
+            let emailregex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+            let passregex = /^(?=.*\d)(?=.*[a-záéíóúüñ]).*[A-ZÁÉÍÓÚÜÑ]/;
+
+            let validateImputs = () => {
+                let errors = []
+                if (name.value.length < 2) {
+                    errors.push(' El nombre debe tener al menos 2 caracteres')
+                }
+                if (nameU.value.length < 2) {
+                    errors.push(' El nombre de usuario debe tener al menos 2 caracteres')
+                }
+                if (!emailregex.test(email.value)) {
+                    errors.push(' El formato de el email es inválido')
+                }
+                if (!passregex.test(pass.value)) {
+                    errors.push('La contraseña debe contener mayúscula y un número y una minuscula')
+                }
+                if (pass.value != pass2.value) {
+                    errors.push('Las contraseñas deben ser iguales')
+                }
+                info.forEach(function (data) {
+                    if (data == email.value) {
+                        errors.push('El email está registrado')
+                    }
+                })
+
+                return errors
+            }
+            form.onsubmit = function (e) {
+                divErrores.innerHTML = "";
+                let errores = validateImputs()
+                console.log(errores);
+                if (errores.length > 0) {
+                    e.preventDefault()
+                    errores.forEach(error => {
+                        divErrores.innerHTML +=
+                            '<div class="alert alert-danger" role="alert">' + error + '</div>'
+                    })
+                } else {
+                    form.submit()
+                }
+            }
+        }
+        )
+})
